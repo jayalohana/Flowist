63d2fb2bfab55c459f12e40b8538c69762fbd0b1
Jaya <jaya.lohana24@gmail.com>
2024-05-06 22:09:46 -0400

    changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 export default clerkMiddleware();
+
+export default authMiddleware({
+  publicRoutes: ["/site", "/api/uploadthing"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
